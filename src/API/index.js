526c263d13168407bd3dbe1c93fe0e971465f83c
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-08-04 01:18:25 +0500

    Fixed API

--- src/API/index.js
+++ src/API/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 export const fetchTrending = async () => {
     try {
-        const {data:{data}} = await axios.get('https://api.giphy.com/v1/gifs/trending?api_key=4A40i9pS4hOK6N3f77WYflnhZe93UIB4&limit=5&rating=g')
+        const {data:{data}} = await axios.get('https://api.giphy.com/v1/gifs/trending?api_key=4A40i9pS4hOK6N3f77WYflnhZe93UIB4&limit=25&rating=g')
         return data;
 
     } catch (err) {
--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Trending.module.css";
 import { Grid } from "@material-ui/core";
 
 export const Trending = ({data:data}) => {
-  data.map((d,i) =>{
-    const {images} = d;
-    if(images){
-      const {downsized_small} = images;
-      console.log(downsized_small);
-    }
-  })
-  
+  const {images} = data[0];
+  const downsized = images ? images.downsized : null;
+  const url = downsized ? downsized.url : null;
+
   return (
     <div>
       <h2 className={styles.trending}>TRENDING Gifs</h2>
-      
+      {
+        data.map((data,i) =>{
+          const {images} = data;
+          const original = images ? images.original : null;
+          const url = original ? original.url : null;
+          return <img key={i} src={url} height="320" width="320" />
+        })
+      }
     </div>
   );
 };
